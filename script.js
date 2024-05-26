410d03d88199ee4dfde5f6d596c288d0e162131f
francesco <francescoferrato@hotmail.com>
2024-05-26 17:04:10 -0400

    Added keys state to event listener so that multiple players can move simulatenously.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const player2 = document.getElementById("game_player2");
 
 const gameBall = document.getElementById("game_ball");
 
+const gameBallHeight = gameBall.getBoundingClientRect().height;
+
 const gameContainer = document.getElementById("game_container");
 
 const gameContainerBorderWidth = 1
@@ -16,104 +18,73 @@ const gameContainerHeight = gameContainer.getBoundingClientRect().height - 2*(ga
 
 const bottomGameConstraint =  gameContainerHeight - playerHeight + distanceChange;
 
+let keys = {};
 
 
+function PlayerMovement(){
+    let player1CurrentPosition = player1.getBoundingClientRect().top - gameBallHeight - gameContainerBorderWidth
+    let player2CurrentPosition = player2.getBoundingClientRect().top - gameBallHeight - gameContainerBorderWidth 
 
+    if(keys["w"]|| keys["W"]){
+        player1CurrentPosition -= distanceChange;
 
-const playerMoveUp = ()=>{
-    console.log("move up");
-}
-
-function playerMove(){
-    
-}
-
-player1.addEventListener("keypress",(event)=>{
-    player1.focus();
-    if(event.key == "w" || event.key == "W"  ){
-
-       
-
-        gameBallHeight = gameBall.getBoundingClientRect().height
+        if(player1CurrentPosition > - distanceChange){
         
-        console.log("before moving up: "+ player1.getBoundingClientRect().top);
+            player1.style.top = player1CurrentPosition + "px";
+        }
+    }
 
-        let cp = player1.getBoundingClientRect().top - gameBallHeight - gameContainerBorderWidth  - distanceChange;
+    if(keys["s"]|| keys["S"] ){
 
-        console.log("moving up: "+ cp);
+        player1CurrentPosition += distanceChange;
 
-        if(cp > - distanceChange){
-            player1.style.top = cp + "px";
+        if(player1CurrentPosition < bottomGameConstraint){
+            player1.style.top = player1CurrentPosition + "px";
+        }
+
+    }
+
+    if(keys["ArrowUp"]){
+        player2CurrentPosition -= distanceChange;
+        
+        if( player2CurrentPosition > - distanceChange){
+            player2.style.top = player2CurrentPosition + "px";
         }
 
 
+    }
 
-        console.log("Player1 current top value: " + player1.style.top);
-
-        console.log(gameContainerHeight);
+    if(keys["ArrowDown"]){
+        player2CurrentPosition += distanceChange;
         
-
-    
-    }else if(event.key == "s" || event.key == "S"){
-
-        gameBallHeight = gameBall.getBoundingClientRect().height
-        
-        console.log("before moving down: "+ player1.getBoundingClientRect().top);
-
-        let cp = player1.getBoundingClientRect().top - gameBallHeight - gameContainerBorderWidth  + distanceChange;
-
-        console.log("moving down: "+ cp);
-
-        if(cp < bottomGameConstraint){
-            player1.style.top = cp + "px";
+        if(player2CurrentPosition < bottomGameConstraint){
+            player2.style.top = player2CurrentPosition + "px";
         }
-       
-
-        console.log("Player1 current top value: " + player1.style.top);
-        
-        console.log(bottomGameConstraint);
        
     }
-});
+}
 
-player2.addEventListener("keydown",(event)=>{
-    player2.focus();
-    
-    if(event.key == "ArrowUp"  ){
-        gameBallHeight = gameBall.getBoundingClientRect().height
-        
-        console.log("before moving up: "+ player2.getBoundingClientRect().top);
 
-        let cp = player2.getBoundingClientRect().top - gameBallHeight - gameContainerBorderWidth  - distanceChange;
 
-        console.log("moving up: "+ cp);
+window.addEventListener("keydown",(event)=>{
+        keys[event.key] = true;
+        PlayerMovement();
 
-        if(cp > - distanceChange){
-            player2.style.top = cp + "px";
-        }
+})
 
-        console.log("Player2 current top value: " + player2.style.top);
+window.addEventListener("keyup",(event)=>{
+    keys[event.key] = false
+})
 
-        console.log(gameContainerHeight);
-        
-    }else if(event.key == "ArrowDown" ){
 
-        gameBallHeight = gameBall.getBoundingClientRect().height
-        
-        console.log("before moving down: "+ player2.getBoundingClientRect().top);
 
-        let cp = player2.getBoundingClientRect().top - gameBallHeight - gameContainerBorderWidth  + distanceChange;
 
-        console.log("moving down: "+ cp);
 
-        if(cp < bottomGameConstraint){
-            player2.style.top = cp + "px";
-        }
-       
 
-        console.log("Player2 current top value: " + player2.style.top);
-        
-        console.log(bottomGameConstraint);
-    }
-});
+
+
+
+
+
+
 
